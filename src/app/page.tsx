b12f75800e6e067935bdd0a3d7f237ed30b75725
Zenathannn <zenathandiancora@gmail.com>
2026-06-18 22:37:31 +0700

    /dashboard to /auth/login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function HomePage() {
   const router = useRouter();
   React.useEffect(() => {
-    router.replace('/dashboard')
+    router.replace('/auth/login')
   }, [router]);
   return null;
 }
